06cee70a43fced73bad64b9f781088088be9d77e
lucemorato <63071872+lucemorato@users.noreply.github.com>
2022-05-30 19:05:25 +0200

    Add files via upload

---
+++ omdb/system/control/FetchPage.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Katija Jurić i Lucija Mikulić
+ * @copyright 2022
+ */
+
+class FetchPage extends AbstractPage {
+
+    public function code() {
+
+        $this->templateName = 'fetch';
+    }
+
+}
